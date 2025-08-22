fb883198fa268082cceae29305a4812de051e031
rahulgote4 <goterahul2000@gmail.com>
2025-08-22 21:59:12 +0530

    change make in TaxGroup class

--- src/com/definelabs/invoice/TaxGroup.java
+++ src/com/definelabs/invoice/TaxGroup.java
@@ -12,6 +12,12 @@ public class TaxGroup {
 	private List<Tax> taxes=new ArrayList<>();
 	
 	
+	@Override
+	public String toString() {
+		return "TaxGroup [name=" + name + ", taxes=" + taxes + "]";
+	}
+
+
 	public String getName() {
 		return name;
 	}
@@ -42,12 +48,12 @@ public class TaxGroup {
 	
 	public double calculateTax(double price)
 	{
-		double totaltax=0.0;
+		double totalTax=0.0;
 		  // Loop through each tax in the group and calculate contribution
 		for(Tax t:taxes)
 		{
-			totaltax +=(price*t.getPercent()/100);
+			totalTax +=(price*t.getPercent()/100);
 		}
-		return totaltax;
+		return totalTax;
 	}
 }
